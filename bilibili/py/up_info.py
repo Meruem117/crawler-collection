cabae126da4e58ea2df159d9b5c98790ec0502f7
Meruem117 <1572677319@qq.com>
2022-04-23 22:51:34 +0800

    update up

--- bilibili/py/up_info.py
+++ bilibili/py/up_info.py
@@ -1,39 +1,51 @@
 import requests
 import json
-import csv
 import constant
 
 
-def get_up_info(mid: str):
+def get_up_info(mid: str) -> None:
+    """
+    get up info by mid
+
+    :param mid: up id
+    :return: up info
+    """
+
     url = 'https://api.bilibili.com/x/web-interface/card?mid=' + mid
     response = requests.get(url=url, headers=constant.HEADERS)
     dataset = json.loads(response.text)
     # data = json.dumps(dataset, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
     # print(data)
 
-    card = dataset.get('data').get('card')
-    mid = card.get('mid')
-    name = card.get('name')
-    face = card.get('face')
-    gender = card.get('sex')
-    fans = card.get('fans')
-    sign = card.get('sign')
-    title = card.get('official_verify').get('desc')
-    u_data.append([mid, name, face, gender, fans, sign, title])
-    # print(u_data)
+    # data
+    data = dataset.get('data')
+    # base info
+    card = data.get('card')
+    name: str = card.get('name')
+    gender: str = card.get('sex')
+    avatar: str = card.get('face')
+    fans: int = card.get('fans')
+    attention: int = card.get('attention')
+    sign: str = card.get('sign')
+    # official description
+    official = card.get('Official')
+    title = official.get('title')
+    desc = official.get('desc')
+    # statistics
+    video_num = data.get('archive_count')
+    article_num = data.get('article_count')
+    follower_num = data.get('follower')
+    like_num = data.get('like_num')
 
 
 if __name__ == '__main__':
-    # params: mid
-    # 'https://api.bilibili.com/x/web-interface/card?mid=?'
-    u_data = []
-    up_id = ''
+    up_id = '12890453'
     get_up_info(up_id)
-    path = './data/ups.csv'
+    # path = './data/ups.csv'
 
-    with open(path, 'a', encoding="utf-8", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        # first create needed
-        # writer.writerow(['mid', 'name', 'face', 'gender', 'fans', 'sign', 'title'])
-        writer.writerows(u_data)
-    print('finished')
+    # with open(path, 'a', encoding="utf-8", newline="") as csv_file:
+    #     writer = csv.writer(csv_file)
+    #     # first create needed
+    #     # writer.writerow(['mid', 'name', 'face', 'gender', 'fans', 'sign', 'title'])
+    #     writer.writerows(u_data)
+    # print('finished')

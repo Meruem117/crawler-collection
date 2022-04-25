934bed6ac889253ff5132f147d4e619951183a21
Meruem117 <1572677319@qq.com>
2022-04-25 23:40:53 +0800

    update douban

--- douban/douban.py
+++ douban/douban.py
@@ -5,7 +5,7 @@ import time
 import constant
 
 
-def common_search(key):
+def search_common(key):
     search_url = 'https://www.douban.com/search'
     param = {
         'q': key
@@ -27,5 +27,25 @@ def common_search(key):
     print(urls)
 
 
+def search_movie(key):
+    url = 'https://search.douban.com/movie/subject_search'
+    params = {
+        'search_text': key
+    }
+    response = requests.get(url=url, params=params, headers=constant.HEADERS)
+    html = response.text.encode("utf-8")
+    soup = BeautifulSoup(html, 'html.parser')
+    urls = []
+    for item in soup.find_all('div', class_="title"):
+        sp = BeautifulSoup(str(item), 'html.parser')
+        if sp.find_all('span') and sp.find_all('span')[0].text == '[剧集]':
+            a = sp.find_all('a')[0]
+            name = a.text
+            if key.split()[0] == name.split()[0]:
+                h = a.get('href')
+                urls.append(h)
+    print(urls)
+
+
 if __name__ == '__main__':
-    common_search("hhh")
+    search_common("hhh")
--- douban/py/get_url.py
+++ douban/py/get_url.py
@@ -2,26 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_url_movie(st):
-    search_url = 'https://search.douban.com/movie/subject_search'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36 Edg/89.0.774.50'
-    }
-    params = {
-        'search_text': st
-    }
-    response = requests.get(url=search_url, params=params, headers=headers)
-    html = response.text.encode("utf-8")
-    soup = BeautifulSoup(html, 'html.parser')
-    for item in soup.find_all('div', class_="title"):
-        sp = BeautifulSoup(str(item), 'html.parser')
-        if sp.find_all('span') and sp.find_all('span')[0].text == '[剧集]':
-            a = sp.find_all('a')[0]
-            name = a.text
-            if st.split()[0] == name.split()[0]:
-                h = a.get('href')
-                target_urls.append(h)
-                print(h)
+
 
 
 if __name__ == '__main__':
